e58588f9beba7a9a73f92fafd005061c236dfb57
Huang Zihao <huangzihaohzh@163.com>
2019-10-11 19:40:58 +0800

    完成了除makeExerciseByStr函数的书写

--- src/entity/Exercise.py
+++ src/entity/Exercise.py
@@ -1,5 +1,6 @@
 import numpy
-
+import random
+import fractions
 # 题目类
 # 用于生成存储题目、答案
 
@@ -13,13 +14,36 @@ class Exercise:
     def __init__(self):
         pass
 
-    # 按range = 10 生成一道题目
+    # 按range生成一道题目
     def makeExercise(self):
-        pass
+        operatingFigureList = []  # 用于存放生成练习式子的数字
+        operationCodeList = []  # 数学式中的运算代码，代表关系：0：+，1：-，2：*，3：÷，4：/
+        operatingFigureNum = random.randint(2, 4)  # 操作数的个数
+        operationCodeNum = operatingFigureNum - 1  # 运算操作的个数
+        # 用随机数生成用于运算的数字
+        i = 1
+        while i <= operatingFigureNum:
+            operatingFigureList.append(random.randint(0, self.range))
+            i += 1
+        # 生成运算代码
+        i = 1
+        while i <= operationCodeNum:
+            operationCodeList.append(random.randint(0, 4))
+            i += 1
+        self.fourOp(operatingFigureList,operationCodeList)
 
-    # 按给定的range和num生成一道题目
+    # 按给定的range和num生成num道题目
     def makeExerciseWithNumAndRange(self,num,range):
-        pass
+        self.range = range
+        exerciseList = []
+        i = 1
+        while i <= num:
+            exercise = Exercise()
+            exercise.makeExercise()
+            exerciseList.append(exercise)
+            i += 1
+        return exerciseList
+
 
     # 按给定的题目字符串
     def makeExerciseByStr(self,exeStr,ansStr):
@@ -27,5 +51,85 @@ class Exercise:
 
     # 以字符串的形式返回题目
     def getExerciseStr(self):
-        pass
+        return self.exerciseStr
 
+    # 以字符串形式返回答案
+    def getAnswerStr(self):
+        return self.answerStr
+
+    """
+    四则运算函数
+    参数：opFigureList    包含参与运算的数的列表
+          opCodeList      包含参与运算的符号，代表关系：0：+，1：-，2：*，3：÷，4：/
+    """
+    def fourOp(self,opFigureList = [],opCodeList = []):
+        # 进行四则运算
+        opFigureTempList = opFigureList.copy()
+        opCodeTempList = opCodeList.copy()
+        answer = 0
+        i = 0
+        while i < len(opCodeList):
+            codeIndex = 1  # 运算符索引
+            # 若有乘除法运算，则先做乘除法
+            if (2 in opCodeTempList or 3 in opCodeTempList or 4 in opCodeTempList):
+                # 遍历操作符列表，查找第一个乘法或除法
+                for opCode in opCodeTempList:
+                    # 若为乘法
+                    if (opCode == 2):
+                        # 从运算数临时列表中取出两个操作数
+                        figure1 = opFigureTempList[codeIndex - 1]
+                        figure2 = opFigureTempList[codeIndex]
+                        # 从运算数临时列表中删掉两个操作数中的后者
+                        del opFigureTempList[codeIndex]
+                        opFigureTempList[codeIndex - 1] = figure1 * figure2
+                    # 若为除法或分数
+                    if (opCode == 3 or opCode == 4):
+                        figure1 = opFigureTempList[codeIndex - 1]
+                        figure2 = opFigureTempList[codeIndex]
+                        del opFigureTempList[codeIndex]
+                        if(figure2 != 0):
+                            opFigureTempList[codeIndex - 1] = figure1 / figure2
+                        else:
+                            answer = None
+                            return
+                    # 从运算符临时列表中删除已经进行运算的运算符
+                    del opCodeTempList[codeIndex - 1]
+                    break
+            else:
+                # opcodeTempList不包含乘除法或做完乘除法后,做加减法
+                for opCode in opCodeTempList:
+                    # 从运算数临时列表中取出两个操作数
+                    figure1 = opFigureTempList[codeIndex - 1]
+                    figure2 = opFigureTempList[codeIndex]
+                    # 从运算数临时列表中删掉两个操作数中的后者
+                    del opFigureTempList[codeIndex]
+                    # 若为加法
+                    if (opCode == 0):
+                        opFigureTempList[codeIndex - 1] = figure1 + figure2
+                    # 若为减法
+                    if (opCode == 1):
+                        opFigureTempList[codeIndex - 1] = figure1 - figure2
+                    # 从运算符临时列表中删除已经进行运算的运算符
+                    del opCodeTempList[codeIndex - 1]
+                    break
+            i += 1
+        answer = opFigureTempList[0]
+        # 生成练习字符串
+        opCodeIndex = 0
+        for opFigure in opFigureList:
+            self.exerciseStr += str(opFigure)
+            if (opCodeIndex < len(opCodeList)):
+                if (opCodeList[opCodeIndex] == 0):
+                    self.exerciseStr += "+"
+                if (opCodeList[opCodeIndex] == 1):
+                    self.exerciseStr += "-"
+                if (opCodeList[opCodeIndex] == 2):
+                    self.exerciseStr += "*"
+                if (opCodeList[opCodeIndex] == 3):
+                    self.exerciseStr += "÷"
+                if (opCodeList[opCodeIndex] == 4):
+                    self.exerciseStr += "/"
+            opCodeIndex += 1
+        # 生成答案字符串
+        self.answerStr = str(fractions.Fraction(answer))
+        return answer
